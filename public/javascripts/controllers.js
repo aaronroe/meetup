f2340c1aa24d7e359ff3acc4a300a7d3894e23cb
aaronroe <aaronalkan@gmail.com>
2014-02-15 16:10:05 -0600

    Adds sample Topics

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -2,22 +2,30 @@ var meetupApp = angular.module('meetupApp', []);
 
 meetupApp.controller('propositionCtrl', function($scope, $http) {
 
+    var closeAllResults = function() {
+        $scope.namesOpen = false;
+        $scope.locationsOpen = false;
+        $scope.topicsOpen = false;
+    }
+
     // this is what will perform search over locations.
     var locationSearcher;
     // declare the list for the locations.
     var locationList;
     // whether or not the locations typeahead should be open
     $scope.locationsOpen = false;
-
+    // helper function that closes all results and opens the desired one.
+    $scope.openLocationsResults = function () {
+        closeAllResults();
+        $scope.locationsOpen = true;
+    };
     // init the location input.
     $scope.locationInput = "";
-
     // action to take if a location option is selected.
     $scope.selectLocationOption = function(location) {
         $scope.locationInput = location.name;
         $scope.locationsOpen = false;
     };
-
     // get the json with the locations.
     $http.get('api/locations.json').then(function(result) {
         // set the fuse searcher.
@@ -31,7 +39,6 @@ meetupApp.controller('propositionCtrl', function($scope, $http) {
         locationList = result.data;
         $scope.locationSearchResults = locationList;
     });
-
     // watch the location input and change the search results accordingly.
     $scope.$watch('locationInput', function(newValue, oldValue) {
         if (locationSearcher !== undefined) {
@@ -41,6 +48,50 @@ meetupApp.controller('propositionCtrl', function($scope, $http) {
             } else {
                 // get search results for what someone types in.
                 $scope.locationSearchResults = locationSearcher.search(newValue);
+            }
+        }
+    });
+
+    // this is what will perform search over topics.
+    var topicSearcher;
+    // declare the list for the topics.
+    var topicList;
+    // whether or not the topics typeahead should be open
+    $scope.topicsOpen = false;
+    // helper function that closes all results and opens the desired one.
+    $scope.openTopicsResults = function () {
+        closeAllResults();
+        $scope.topicsOpen = true;
+    };
+    // init the location input.
+    $scope.topicInput = "";
+    // action to take if a topic option is selected.
+    $scope.selectTopicOption = function(location) {
+        $scope.topicInput = location.name;
+        $scope.topicsOpen = false;
+    };
+    // get the json with the topics.
+    $http.get('api/topics.json').then(function(result) {
+        // set the fuse searcher.
+        var options = {
+          keys: ['name']
+        }
+
+        topicSearcher = new Fuse(result.data, options);
+
+        // init the list of topics and the topic search results.
+        topicList = result.data;
+        $scope.topicSearchResults = locationList;
+    });
+    // watch the topic input and change the search results accordingly.
+    $scope.$watch('topicInput', function(newValue, oldValue) {
+        if (locationSearcher !== undefined) {
+            // check to see if new value is an empty string
+            if (newValue === "") {
+                $scope.topicSearchResults = locationList;
+            } else {
+                // get search results for what someone types in.
+                $scope.topicSearchResults = topicSearcher.search(newValue);
             }
         }
     });
