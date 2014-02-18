cf383753a6868a2d6a996a21e256694c6c8bebc8
aaronroe <aaronalkan@gmail.com>
2014-02-18 11:22:31 -0600

    Adds max length to proposition form variables

--- app/models/forms/Proposition.java
+++ app/models/forms/Proposition.java
@@ -1,14 +1,19 @@
 package models.forms;
 
+import play.data.validation.Constraints;
 import play.data.validation.Constraints.Required;
 
 /**
  * Represents the form for a proposition.
  */
 public class Proposition {
+    @Constraints.MaxLength(100)
     public String name;
+    @Constraints.MaxLength(100)
     public String email;
+    @Constraints.MaxLength(100)
     public String location;
+    @Constraints.MaxLength(100)
     public String topic;
 
     public String validate() {
