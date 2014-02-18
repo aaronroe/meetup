e0255b4dbeab68f7469b054c47b501f2b91791c2
aaronroe <aaronalkan@gmail.com>
2014-02-17 20:18:29 -0600

    Adds basic invitation model

--- app/models/Invitation.java
+++ app/models/Invitation.java
@@ -1,8 +1,54 @@
 package models;
 
+import play.db.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * Class that represents an invitation.
  */
-public class Invitation {
+@Entity
+public class Invitation extends Model {
+
+    /**
+     * Id of the invitation in the database.
+     */
+    @Id
+    private long id;
+
+    /**
+     * Code for verifying invitations.
+     */
+    private String verificationCode;
+
+    /**
+     * Whether or not a user has responded to the invitation yet.
+     */
+    private boolean responded;
+
+    /**
+     * Finder for invitations.
+     */
+    public Finder<Long, Invitation> find = new Finder<Long, Invitation>(Long.class, Invitation.class);
+
+    /**
+     * Default constructor for invitation.
+     */
+    public Invitation() {
+        this.verificationCode = "";
+        this.responded = false;
+    }
+
+    /**
+     * Static method that creates an invitation and encapsulates saving.
+     * @return The newly created invitation.
+     */
+    public static Invitation create() {
+        Invitation invitation = new Invitation();
+        invitation.save();
+
+        return invitation;
+    }
 
 }
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,27 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table invitation (
+  id                        bigint not null,
+  verification_code         varchar(255),
+  responded                 boolean,
+  constraint pk_invitation primary key (id))
+;
+
+create sequence invitation_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists invitation;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists invitation_seq;
+
