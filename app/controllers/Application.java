c08f82d9936730827e4c817422e68b12c3b3a5f2
aaronroe <aaronalkan@gmail.com>
2014-02-23 18:49:42 -0600

    Adds invisible string at bottom of emails to prevent gmail trimming

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -19,6 +19,7 @@ import views.html.*;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Random;
 
 import static play.data.Form.form;
 import static play.mvc.Results.badRequest;
@@ -88,11 +89,9 @@ public class Application extends JavaController {
                             // create the invitation in the database.
                             Invitation invitation = Invitation.create(inviterName, proposition.name, netId+"@rice.edu", proposition.email, when2MeetUrl, proposition.location, proposition.topic);
 
-                            Calendar currentDate = new GregorianCalendar();
-                            String year = Integer.toString(currentDate.get(Calendar.YEAR));
-                            String month = String.format("%02d", currentDate.get(Calendar.MONTH) + 1); // months indexed at 0.
-                            String day = String.format("%02d", currentDate.get(Calendar.DAY_OF_MONTH));
-                            String date = day+"-"+month+"-"+year;
+                            // generate a random number to put at the bottom of the mail.
+                            Random random = new Random();
+                            int randomInt = random.nextInt(2147483647);
 
                             MailerAPI mail = play.Play.application().plugin(MailerPlugin.class).email();
                             mail.setSubject("You have received a Rice Meetup Invite!");
@@ -103,7 +102,8 @@ public class Application extends JavaController {
                                     "\t<h1 style=\"color: #333\">I would love to buy you a drink from <i>"+proposition.location+"</i> sometime and talk about <i>"+proposition.topic+"</i>.</h1>\n" +
                                     "\t<button style=\"display: inherit;background-color: #47a447;padding: 10px 20px;margin:10px 0;border-radius: 4px;border: 1px solid transparent;font-size: 14px;cursor: pointer;\"><a href=\""+invitation.getAcceptURL()+"\" style=\"color: #fff;text-decoration: none;\" href=\"\">Sounds great! When would you like to meet?</a></button>\n" +
                                     "\t<button style=\"display: inherit;background-color: #d9534f;padding: 10px 20px;margin:10px 0;border-radius: 4px;border: 1px solid transparent;font-size: 14px;cursor: pointer;\"><a href=\""+invitation.getRejectURL()+"\" style=\"color: #fff;text-decoration: none;\" href=\"\">Sorry, I'm busy and can't meet! Maybe some other time.</a></button>\n" +
-                                    "\t<h4 style=\"color: #333\">Sent on "+date+" | Powered by <a href=\"http://meetup.riceapps.org\">Rice Meetup</a></h4>\n" +
+                                    "\t<h4 style=\"color: #333\">Powered by <a href=\"http://meetup.riceapps.org\">Rice Meetup</a></h4>\n" +
+                                    "\t<span style=\"color: #fff;;\">"+randomInt+"</span>" +
                                     "</html>" );
 
                             return ok();
@@ -127,12 +127,9 @@ public class Application extends JavaController {
             // set the invitation as responded.
             invitation.setResponded(true);
 
-            // get the current date.
-            Calendar currentDate = new GregorianCalendar();
-            String year = Integer.toString(currentDate.get(Calendar.YEAR));
-            String month = String.format("%02d", currentDate.get(Calendar.MONTH) + 1); // months indexed at 0.
-            String day = String.format("%02d", currentDate.get(Calendar.DAY_OF_MONTH));
-            String date = day+"-"+month+"-"+year;
+            // generate a random number to put at the bottom of the mail.
+            Random random = new Random();
+            int randomInt = random.nextInt(2147483647);
 
             // send a confirmation email to the invited.
             MailerAPI mail = play.Play.application().plugin(MailerPlugin.class).email();
@@ -145,8 +142,11 @@ public class Application extends JavaController {
                     "\t<h1 style=\"color: #333\">"+invitation.getInviterName()+" offered to buy you a drink from <i>"+invitation.getLocation()+"</i> in order to talk about <i>"+invitation.getTopic()+"</i>.</h1>\n" +
                     "\t<h2 style=\"color: #333\">Here is the <a href=\""+invitation.getWhen2MeetURL()+"\">when2meet</a> that you can use for scheduling. Alternatively you can just reply to this email to talk.</h2>\n" +
                     "\t<h2 style=\"color: #333\">Hope things work out!</h2>\n" +
-                    "\t<h4 style=\"color: #333\">Sent on \"+date+\" | Powered by <a href=\"http://meetup.riceapps.org\">Rice Meetup</a></h4>\n" +
+                    "\t<h4 style=\"color: #333\">Powered by <a href=\"http://meetup.riceapps.org\">Rice Meetup</a></h4>\n" +
+                    "\t<span style=\"color: #fff;;\">"+randomInt+"</span>" +
                     "</html>");
+
+            randomInt = random.nextInt(2147483647);
 
             // send the notification email to the inviter.
             mail.setSubject("Your Rice Meetup invite has been accepted!");
@@ -158,7 +158,8 @@ public class Application extends JavaController {
                     "\t<h1 style=\"color: #333\">To jog your memory, you offered to buy a drink from <i>"+invitation.getLocation()+"</i> in order to talk about <i>"+invitation.getTopic()+"</i>.</h1>\n" +
                     "\t<h2 style=\"color: #333\">Here is a <a href=\""+invitation.getWhen2MeetURL()+"\">when2meet</a> to schedule your meet up.</h2>\n" +
                     "\t<h2 style=\"color: #333\">Hope things work out!</h2>\n" +
-                    "\t<h4 style=\"color: #333\">Sent on \"+date+\" | Powered by <a href=\"http://meetup.riceapps.org\">Rice Meetup</a></h4>\n" +
+                    "\t<h4 style=\"color: #333\">Powered by <a href=\"http://meetup.riceapps.org\">Rice Meetup</a></h4>\n" +
+                    "\t<span style=\"color: #fff;;\">"+randomInt+"</span>" +
                     "</html>");
 
             return ok(response.render("You have successfully accepted your invitation to meet up!", true, invitation.getWhen2MeetURL()));
@@ -177,6 +178,10 @@ public class Application extends JavaController {
             // set the invitation as responded and send a notification email.
             invitation.setResponded(true);
 
+            // generate a random number to put at the bottom of the mail.
+            Random random = new Random();
+            int randomInt = random.nextInt(2147483647);
+
             MailerAPI mail = play.Play.application().plugin(MailerPlugin.class).email();
             mail.setSubject("Your Rice Meetup invite has been declined...");
             mail.setRecipient(invitation.getInviterEmail());
@@ -185,7 +190,8 @@ public class Application extends JavaController {
                     "\t<br />\n" +
                     "\t<h1 style=\"background-color: #d9534f;display: inline;padding: .2em .6em .3em;font-weight: 700;color: #fff;text-align: center;border-radius: .25em;\">Sadly, "+invitation.getInvitedName()+" has declined your invite...</h1>\n" +
                     "\t<h2 style=\"color: #333\">Sorry and hope things work out next time!</h2>\n" +
-                    "\t<h4 style=\"color: #333\">Sent on \"+date+\" | Powered by <a href=\"http://meetup.riceapps.org\">Rice Meetup</a></h4>\n" +
+                    "\t<h4 style=\"color: #333\">Powered by <a href=\"http://meetup.riceapps.org\">Rice Meetup</a></h4>\n" +
+                    "\t<span style=\"color: #fff;;\">"+randomInt+"</span>" +
                     "</html>");
 
             return ok(response.render("You have successfully rejected your invitation to meet up...", false, invitation.getWhen2MeetURL()));
